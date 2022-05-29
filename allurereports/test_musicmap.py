c03cf12d0ff7d73bc267d3599dcede26497e8c7d
AlexeyZh <waltafunk@gmail.com>
2022-05-29 13:55:39 +0300

    add def test_title

--- allurereports/test_musicmap.py
+++ allurereports/test_musicmap.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import allure
 import pytest
 
+
 class TestMusicMaP:
 
-    def test_Logo(selfself):
+    def test_title(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.music-map.com/")
+        status = self.driver.find_element(By.XPATH, '//*[@id="the_title"]').is_displayed()
+        if status == True:
+            assert True
+        else:
+            assert False
+        self.driver.close()
