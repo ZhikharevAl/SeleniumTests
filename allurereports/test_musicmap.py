ad243a243bdfa72ec6a9fca83af712eca6741f00
AlexeyZh <waltafunk@gmail.com>
2022-05-29 19:40:13 +0300

    add allure. Severity

--- allurereports/test_musicmap.py
+++ allurereports/test_musicmap.py
@@ -1,12 +1,14 @@
+import allure
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import allure
 from allure_commons.types import AttachmentType
-import pytest
 
 
+@allure.severity(allure.severity_level.NORMAL)
 class TestMusicMaP:
 
+    @allure.severity(allure.severity_level.MINOR)
     def test_title(self):
         self.driver = webdriver.Chrome()
         self.driver.get("https://www.music-map.com/")
@@ -17,22 +19,18 @@ class TestMusicMaP:
             assert False
         self.driver.close()
 
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_search(self):
         self.driver = webdriver.Chrome()
         self.driver.get("https://www.music-map.com/")
         self.driver.find_element(By.ID, 'f').send_keys("-1")
         self.driver.find_element(By.XPATH, '//*[@id="search_form"]/button').click()
-        act_title = self.driver.find_element(By.XPATH, '//*[@id="s0"]')
+        act_title = self.driver.title
 
-        if act_title == self.driver.find_element(By.XPATH, '//*[@id="s0"]'):
+        if act_title == "Music like Red - Similar Bands and Artists":
             self.driver.close()
             assert True
         else:
-            allure.attach(self.driver.get_screenshot_as_png(), name="test_search", attachment_type=AttachmentType.png)
+            allure.attach(self.driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
             self.driver.close()
             assert False
-
-
-
-
-
